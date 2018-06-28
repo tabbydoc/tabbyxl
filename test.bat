b14f2b464ba55254d3f8b9a264b4fe33c22b306b
Alexey Shigarov <shigarov@gmail.com>
2018-06-28 11:19:11 +0800

    Clarifying the command files

--- test.bat
+++ test.bat
@@ -1,1 +1,1 @@
-java -jar target\TabbyXL-0.1-jar-with-dependencies.jar -input examples\test\smpl.xlsx -ruleset examples\test\smpl.dslr -output examples\test\results
+java -jar target\TabbyXL2-1.0-jar-with-dependencies.jar -input examples\data\smpl.xlsx -ruleset examples\rules\smpl.dslr -output examples\results
--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-java -jar target/TabbyXL-0.1-jar-with-dependencies.jar -input samples/sampl.xlsx -ruleset samples/sampl.dslr -output samples/results
+java -jar target/TabbyXL2-1.0-jar-with-dependencies.jar -input examples/data/smpl.xlsx -ruleset examples/rules/smpl.dslr -output examples/results
