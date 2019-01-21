44898c3b1463f82ecfdad16db53c2b29cad30285
Viacheslav Paramonov <slv@icc.ru>
2019-01-21 16:57:08 +0800

    Add method for general Tags detection

---
+++ src/main/java/ru/icc/td/tabbyxl/util/CommonAPILoader.java
@@ -0,0 +1,40 @@
+package ru.icc.td.tabbyxl.util;
+
+import ru.icc.td.tabbyxl.commons.LitTypeDetect;
+
+import java.io.IOException;
+import java.util.List;
+
+public class CommonAPILoader {
+
+    public static void main(String[] params) throws IOException, ClassNotFoundException {
+        if (params[0].equals("-s")){
+            //Data from param
+            String delim = "\\s*,\\s*";
+            String[] initData = params[1].split(delim);
+            stanfordCore(initData);
+        }
+
+    }
+
+    private static void stanfordCore(String[] data) throws IOException, ClassNotFoundException {
+        LitTypeDetect ltd = new LitTypeDetect(data);
+        /*
+        for (String s : data){
+
+            ltd.addData(s);
+        }
+        */
+
+        List<String> tags = ltd.getNerTags();
+        for(String tag : tags){
+            System.out.print(tag + " ");
+        }
+
+
+    }
+
+
+}
+
+
