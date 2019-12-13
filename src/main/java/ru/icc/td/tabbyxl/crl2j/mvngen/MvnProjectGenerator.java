9276b8bb5543a66be7afcf26e535781d9762a1de
Alexey Shigarov <shigarov@gmail.com>
2019-12-13 15:09:38 +0800

    Polish

--- src/main/java/ru/icc/td/tabbyxl/crl2j/mvngen/MvnProjectGenerator.java
+++ src/main/java/ru/icc/td/tabbyxl/crl2j/mvngen/MvnProjectGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-19 Alexey O. Shigarov (shigarov@icc.ru) and Vasiliy V. Khristyuk
+ * Copyright 2019 Alexey O. Shigarov (shigarov@icc.ru) and Vasiliy V. Khristyuk
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
