7a098c11a522c28a5e9a30c9d64d4743bc8c0291
Alexey Shigarov <shigarov@gmail.com>
2020-03-01 12:12:06 +0800

    Fix a bug with writing compound values of labels in output excel workbooks

--- src/main/java/ru/icc/td/tabbyxl/crl2j/CodeGenerator.java
+++ src/main/java/ru/icc/td/tabbyxl/crl2j/CodeGenerator.java
@@ -390,6 +390,9 @@ final class CodeGenerator {
                 Action action = actions.next();
 
                 List<Expression> operands = action.getOperands();
+                if (operands.isEmpty()) {
+                    System.err.println("No operands of the action: " + action.toString());
+                }
                 String caller = generateNormalizedExpression(operands.get(0).getTokens(), "");
 
                 code
