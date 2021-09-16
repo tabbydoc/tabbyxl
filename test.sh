0aeabbcda5d37d915f572f08750a147ba47a868f
Alexey O. Shigarov <shigarov@gmail.com>
2021-09-16 18:52:07 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-java -jar target/TabbyXL-1.1.0-jar-with-dependencies.jar -input examples/data/smpl.xlsx -ruleset examples/rules/smpl.crl -output examples/results
+java -jar target/TabbyXL-1.1.1-jar-with-dependencies.jar -input examples/data/smpl.xlsx -ruleset examples/rules/smpl.crl -output examples/results
